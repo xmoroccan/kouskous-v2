5c1c596539ede1cf3e47d7b5608e63619f41d9f2
Yassine Sedrani <cxmoro@gmail.com>
2020-01-01 20:30:50 +0100

    deploy first shoot

---
+++ precache-manifest.dd8218c3a0a4dfff062dc4e3ef9fd531.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f7d9b461e018c0f6c8a",
+    "url": "/kouskous-v2/css/app.3e83bc8b.css"
+  },
+  {
+    "revision": "ecf9ce7448543aa0b90d",
+    "url": "/kouskous-v2/css/chunk-vendors.b0a840e2.css"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/kouskous-v2/fonts/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/kouskous-v2/fonts/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/kouskous-v2/fonts/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/kouskous-v2/fonts/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/kouskous-v2/fonts/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/kouskous-v2/fonts/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/kouskous-v2/fonts/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/kouskous-v2/fonts/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/kouskous-v2/fonts/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/kouskous-v2/fonts/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/kouskous-v2/fonts/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/kouskous-v2/fonts/fa-solid-900.f6121be5.woff2"
+  },
+  {
+    "revision": "a07068201a087ab24a6b29864818794c",
+    "url": "/kouskous-v2/img/amba-sauce.a0706820.jpg"
+  },
+  {
+    "revision": "94dadd669b7fb7f9b7e74180385ff9f7",
+    "url": "/kouskous-v2/img/beverages.94dadd66.jpg"
+  },
+  {
+    "revision": "d72293118cda50ec50c39957d9d836d0",
+    "url": "/kouskous-v2/img/fa-brands-400.d7229311.svg"
+  },
+  {
+    "revision": "d2e53334c22a9a4937bc26e84b36e1e0",
+    "url": "/kouskous-v2/img/fa-regular-400.d2e53334.svg"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "/kouskous-v2/img/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "0464628936dfe8bc01ec9cb4f115d7da",
+    "url": "/kouskous-v2/img/fattoush-salad.04646289.jpg"
+  },
+  {
+    "revision": "fdfbd23c9521dcf4e54254c74fb31bc9",
+    "url": "/kouskous-v2/img/footer-bg.fdfbd23c.svg"
+  },
+  {
+    "revision": "1aee651ae412ba05e853a5c68afa2299",
+    "url": "/kouskous-v2/img/group.1aee651a.png"
+  },
+  {
+    "revision": "341fda1d7fb2ab76e411976611481066",
+    "url": "/kouskous-v2/img/header.341fda1d.jpg"
+  },
+  {
+    "revision": "c549239a1a0d88e1a6d974ed86cc6cc0",
+    "url": "/kouskous-v2/img/imw-logo.c549239a.png"
+  },
+  {
+    "revision": "d3eff2b129e92b8699461707124fbdf5",
+    "url": "/kouskous-v2/img/logo.d3eff2b1.png"
+  },
+  {
+    "revision": "fa10e90e903d9e9c92b0b47c81f20cf2",
+    "url": "/kouskous-v2/img/logo.fa10e90e.svg"
+  },
+  {
+    "revision": "3fa56147dc2da2f3d5182d295f91324e",
+    "url": "/kouskous-v2/img/moroccan-pack.3fa56147.jpg"
+  },
+  {
+    "revision": "f10c2926854de32f2419fd62639a652d",
+    "url": "/kouskous-v2/img/special-event.f10c2926.png"
+  },
+  {
+    "revision": "3b1050b81c39a0f27d933ea4268f2163",
+    "url": "/kouskous-v2/img/tagine-bg.3b1050b8.jpeg"
+  },
+  {
+    "revision": "97676f299a33b27ec664b23d7e82ef57",
+    "url": "/kouskous-v2/index.html"
+  },
+  {
+    "revision": "613d314b13815d1a01e7",
+    "url": "/kouskous-v2/js/about.c4dd08f9.js"
+  },
+  {
+    "revision": "4f7d9b461e018c0f6c8a",
+    "url": "/kouskous-v2/js/app.f92d46fa.js"
+  },
+  {
+    "revision": "ecf9ce7448543aa0b90d",
+    "url": "/kouskous-v2/js/chunk-vendors.c905587d.js"
+  },
+  {
+    "revision": "e229bb76392ee3ce346b3071d58a2f1c",
+    "url": "/kouskous-v2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/kouskous-v2/robots.txt"
+  }
+]);
